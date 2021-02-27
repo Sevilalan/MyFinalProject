b6c46d95afa5a1f5d731a9477abd7720d7d3733d
Sevilalan <61330327+Sevilalan@users.noreply.github.com>
2021-02-28 00:30:53 +0300

    28.02.2021

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -11,5 +11,7 @@ namespace Business.Constants
         public static string ProductNameInvalid = "Ürün ismi geçersiz";
         public static string MaintenanceTime="Sistem bakımda";
         public static string ProductsListed="Ürünler listelendi";
+        public static string UnitPriceInvalid = "Fiyat geçersiz";
+
     }
 }
